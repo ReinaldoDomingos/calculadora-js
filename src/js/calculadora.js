ce9f9b0e3a7f91cd144002af2f72a3b650797a93
ReinaldoDomingos <reijunior100@gmail.com>
2020-06-09 18:01:29 -0400

    Refatorando código

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,30 +1,50 @@
 var Calculadora = {
-    adicionar: function (num1, num2) {
-        if (isNaN(num1) || isNaN(num2))
-            return 0;
+    ADICAO: '+',
+    SUBTRACAO: '-',
+    DIVISAO: '/',
+    MULTIPLICACAO: '*',
 
-        return parseFloat(num1) + parseFloat(num2);
+    adicionar: function (num1, num2) {
+        return Calculadora.calcular(num1, num2, Calculadora.ADICAO);
     },
     subtrair: function (num1, num2) {
-        if (isNaN(num1) || isNaN(num2))
-            return 0;
-
-        return num1 - num2
+        return Calculadora.calcular(num1, num2, Calculadora.SUBTRACAO);
     },
     dividir: function (num1, num2) {
-        if (isNaN(num1) || isNaN(num2))
-            return 0;
-
         if (num2 == 0)
             return 'Erro'
-        return num1 / num2;
+        return Calculadora.calcular(num1, num2, Calculadora.DIVISAO);
     },
     multiplicar: function (num1, num2) {
+        return Calculadora.calcular(num1, num2, Calculadora.MULTIPLICACAO);
+    },
+    calcular: function (num1, num2, operacao) {
+        var result = 0;
+        num1 = parseFloat(num1)
+        num2 = parseFloat(num2)
+
         if (isNaN(num1) || isNaN(num2))
             return 0;
 
-        return num1 * num2;
-    },
+        switch (operacao) {
+            case Calculadora.ADICAO:
+                result = num1 + num2
+                break
+            case Calculadora.SUBTRACAO:
+                result = num1 - num2
+                break
+            case Calculadora.DIVISAO:
+                result = num1 / num2
+                break
+            case Calculadora.MULTIPLICACAO:
+                result = num1 * num2
+                break
+            default:
+                result = 0;
+                break
+        }
+        return result;
+    }
 }
 // usando require no nodejs
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
